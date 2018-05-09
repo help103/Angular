0a7966776a31122dc9292143eff12126f7cd575e
help103 <33193899+help103@users.noreply.github.com>
2018-05-09 18:10:04 +0530

    Update ICustomer.ts

--- app/Customer/ICustomer.ts
+++ app/Customer/ICustomer.ts
@@ -7,3 +7,37 @@ export interface ICustomer
     MobileNo:string;
     CreditLimit:number;
 }
+import { Injectable } from '@angular/core'
+import { Http, Response } from '@angular/http'
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/operator/map'
+
+import { ICustomer } from './ICustomer'
+
+@Injectable()
+export class CustomerService {
+    url: string = "/api/CustomerTbls";
+
+    constructor(private _htt: Http) { }
+    getCustomers(): Observable<ICustomer[]> {
+        return this._htt.get(this.url)
+            .map((resp: Response) => <ICustomer[]>resp.json());
+    }
+
+    getCustById(id: number): Observable<ICustomer> {
+
+        return this._htt.get(this.url + "/" + id)
+            .map((data)=><ICustomer>data.json());
+    }
+
+    updateCustomer(id: number, rec: ICustomer): Observable<Response> {
+        rec.CustomerID = id;
+        return this._htt.put(this.url + "/" + id, rec)
+            .map((data)=>data.json());
+    }
+
+    postCustomer(rec: ICustomer): Observable<Response> {
+        alert(JSON.stringify(rec));
+        return this._htt.post(this.url, rec);
+    }
+}
